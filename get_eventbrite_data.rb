f3d5b70bd7624773d3610311aee25ee8cd5ba759
Stephanie Roth <stephr3@gmail.com>
2020-03-11 18:01:05 +0900

    format attendee data

--- get_eventbrite_data.rb
+++ get_eventbrite_data.rb
@@ -35,13 +35,13 @@ def get_event_list
 	puts "Total Events: " + response_body["pagination"]["object_count"].to_s
 	output_pages_complete(response_body["pagination"])
 	
-	while response_body["pagination"]["has_more_items"]
-		continuation_uri = get_event_list_uri + "&continuation=" + response_body["pagination"]["continuation"]
-		response_body = get_response_body(continuation_uri)
-		raise StandardError.new("There was an error retrieving all events for this search") if !response_body
-		puts response_body["pagination"]["page_number"].to_s + " of " +  response_body["pagination"]["page_count"].to_s + " pages retrieved"
-		event_list.push(response_body["events"])
-	end
+	# while response_body["pagination"]["has_more_items"]
+	# 	continuation_uri = get_event_list_uri + "&continuation=" + response_body["pagination"]["continuation"]
+	# 	response_body = get_response_body(continuation_uri)
+	# 	raise StandardError.new("There was an error retrieving all events for this search") if !response_body
+	# 	puts response_body["pagination"]["page_number"].to_s + " of " +  response_body["pagination"]["page_count"].to_s + " pages retrieved"
+	# 	event_list.push(response_body["events"])
+	# end
 	event_list 
 end
 
@@ -57,24 +57,67 @@ end
 
 def get_attendees(formatted_events)
 	attendees_list = []
-	formatted_events.each do |formatted_event|
+	formatted_events = [formatted_events[0], formatted_events[1]] # remove later
+	formatted_events.each_with_index do |formatted_event, i|
 		response_body = get_response_body(get_attendees_uri(formatted_event[:id]))
 		raise StandardError.new("There was an error retrieving attendees for this search") if !response_body
 		formatted_response = get_formatted_attendees(formatted_event, response_body["attendees"])
-		# attendees_list.push(formatted_response)
+		attendees_list.push(formatted_response)
+		puts (i + 1).to_s + " of " + formatted_events.length.to_s + " events complete" if (i + 1) % 5 == 0
 	end
-	# attendees_list
+	puts attendees_list
 end
 
 def get_formatted_attendees(event, attendees)
 	attendees_list = []
-	puts "Original Number of Attendees: " + attendees.length.to_s
+	attendees = [attendees[0], attendees[1]] # remove later
 	attendees.each do |attendee|
 		next if !attendee["checked_in"]
-		attendees_list.push({"name": attendee["profile"]["name"], "checked_in": attendee["checked_in"]})
+		attendees_list.push(create_attendee(event, attendee))
 	end
-	puts "New number of attendees: " + attendees_list.length.to_s
-	puts attendees_list
+	attendees_list
+end
+
+def create_attendee(event, attendee)
+	is_el ? english_lounge_pep_data(event, attendee) : academic_advising_paw_data(event, attendee)
+end
+
+def english_lounge_pep_data(event, attendee)
+	{
+		"Event Name": event[:name], 
+		"First Name": attendee["profile"]["first_name"],
+		"Last Name": attendee["profile"]["last_name"],
+		"Email": attendee["profile"]["email"],
+		"Ticket Type": attendee["ticket_class_name"],
+		"Date Attending": event[:date],
+		"What is your year/position at TIU?": attendee["answers"][0]["answer"],
+		"What is your major/specialty at TIU?": attendee["answers"][1]["answer"],
+		"Why are you coming to English Lounge?": attendee["answers"][2]["answer"],
+		"Who teaches your CB class?": attendee["answers"][4]["answer"],
+		"Who teaches your EP class?": attendee["answers"][5]["answer"],
+		"Who teaches this class?": attendee["answers"][6]["answer"]
+	}	
+end
+
+def academic_advising_paw_data(event, attendee)
+	{
+		"Event Name": event[:name], 
+		"First Name": attendee["profile"]["first_name"],
+		"Last Name": attendee["profile"]["last_name"],
+		"Email": attendee["profile"]["email"],
+		"Ticket Type": attendee["ticket_class_name"],
+		"Date Attending": event[:date],
+		"What is your year/position at TIU?": attendee["answers"][0]["answer"],
+		"What is your major/specialty at TIU?": attendee["answers"][1]["answer"],
+		"Why are you coming to Academic Advising?": attendee["answers"][2]["answer"],
+		"Who teaches your AC1 class?": attendee["answers"][5]["answer"],
+		"Who teaches your AC2 class?": attendee["answers"][6]["answer"],
+		"Who teaches your EC/BW class?": attendee["answers"][7]["answer"],
+		"Who teaches your CC class?": attendee["answers"][8]["answer"],
+		"Who teaches your CB/BS class?": attendee["answers"][9]["answer"],
+		"Who teaches this class?": attendee["answers"][10]["answer"],
+		"What assignment, project, or topic are you bringing to Academic Advising?": attendee["answers"][3]["answer"]
+	}	
 end
 
 def get_response_body(uri_string)
@@ -116,6 +159,10 @@ def event_type
 	else
 	  input
 	end
+end
+
+def is_el
+	input == "EL" || input == "PEP"
 end
 
 def get_event_list_uri
